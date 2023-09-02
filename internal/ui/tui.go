60f93e7d1af3f22ab9ac30cde5d33d0cdfe4f665
oneseIf <eric.garcia12317@gmail.com>
2023-09-01 20:29:38 -0500

    feat(ui): implement basic todo list tui
    Implement crude todo list tui using the tview library. Allow for basic
    interactions such as moving with arrows and press q to quit.
    
    Implement tui struct and its methods to statisy the basic ui interface.

--- internal/ui/tui.go
+++ internal/ui/tui.go
@@ -1,1 +1,56 @@
 package ui
+
+import (
+	"fmt"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/iuiq/do/internal/tasks"
+	"github.com/rivo/tview"
+)
+
+type TUI struct {
+	list *tview.List
+	app  *tview.Application
+}
+
+// Init intializes the tview app and sets up the UI.
+func (tui *TUI) Init(tl *tasks.TodoList) {
+	tui.app = tview.NewApplication()
+
+	tui.list = tview.NewList().
+		ShowSecondaryText(false)
+
+	// Populate tui list with initial tasks
+	tui.Populate(tl)
+
+	// Set up user input handling
+	tui.list.SetInputCapture(tui.setupInputCapture(tl))
+
+	if err := tui.app.SetRoot(tui.list, true).Run(); err != nil {
+		panic(err)
+	}
+}
+
+func (tui *TUI) Populate(tl *tasks.TodoList) {
+	tui.list.Clear()
+	for _, task := range tl.Tasks() {
+		// Add a task to the list.
+		tui.list.AddItem(task.Name(), "", 0, nil)
+	}
+}
+
+func (tui *TUI) setupInputCapture(tl *tasks.TodoList) func(event *tcell.EventKey) *tcell.EventKey {
+	return func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyRune:
+			switch event.Rune() {
+			case 'a':
+				// TODO: code to run whenever "a" is pressed
+				fmt.Println("Running to code to handle adding a new task...")
+			case 'q':
+				tui.app.Stop()
+			}
+		}
+		return event
+	}
+}
