1d95df69f768a0af9d1724320072667a4d67ae8e
oneseIf <eric.garcia12317@gmail.com>
2023-10-04 18:39:31 -0500

    refactor: simplify main

--- cmd/do/main.go
+++ cmd/do/main.go
@@ -1,4 +1,52 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	s "github.com/iuiq/do/internal/storage"
+	t "github.com/iuiq/do/internal/tasks"
+	"github.com/iuiq/do/internal/ui"
+)
+
+func run() {
+	switch len(os.Args) {
+	case 2:
+		switch os.Args[1] {
+		case "form":
+			ui.InitForm()
+			return
+		}
+	}
+
+	yamlPath := os.Getenv("DO_DATA_PATH")
+	if len(yamlPath) == 0 {
+		log.Fatal("Environment variable DO_DATA_PATH must be set")
+	}
+
+	store := s.YAMLStorage{Filename: yamlPath}
+
+	list := new(t.TodoList)
+	list.SetTitle("Daily TODOs")
+	if err := store.Load("list", &list); err != nil {
+		log.Fatalf("Error loading list:", err)
+	}
+	tree := new(t.BoardTree)
+	if err := store.Load("boards", &tree); err != nil {
+		log.Fatalf("Error loading boards:", err)
+	}
+
+	tui := new(ui.TUI)
+	tui.Init(list, tree)
+
+	if err := store.Save("list", list); err != nil {
+		log.Fatalf("Error saving list:", err)
+	}
+	if err := store.Save("boards", tree); err != nil {
+		log.Fatalf("Error saving board:", err)
+	}
+}
+
 func main() {
+	run()
 }
